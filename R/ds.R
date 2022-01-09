36a6f47595171d8e22c0b6cc04ce1f2382704314
rulesforpower <63574521+rulesforpower@users.noreply.github.com>
2022-01-09 13:42:27 -0500

    Update ds.R

--- R/ds.R
+++ R/ds.R
@@ -12,5 +12,7 @@ ds <- function(x) {
   #Numeric summary
   data.frame(min = min(x),
              median = median(x),
-             max = max(x))
+             max = max(x),
+            sd = sd(x),
+            range = max(x) - min(x))
 }
